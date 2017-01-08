ead7f8c977a0ce98384cdd318dd5a555a4821dbd
novy <slnowak154@gmail.com>
2017-01-08 16:32:56 +0100

    Got rid path assertions

--- astar_assertions.go
+++ astar_assertions.go
@@ -1,7 +1,6 @@
 package astar
 
 import (
-	"reflect"
 	"testing"
 	"math"
 )
@@ -12,29 +11,4 @@ func AssertDistanceEqual(t *testing.T, actualDistance, expectedDistance float64)
 		t.Errorf("Wrong distance. Got %f, expected %f.", actualDistance, expectedDistance)
 	}
 
-}
-func AssertTraceEqual(t *testing.T, actualTrace []Node, expectedTrace []AStarNodeState) {
-	assertTraceAsNodeStateEqual(t, toAStarNodeState(actualTrace), expectedTrace)
-}
-
-func assertTraceAsNodeStateEqual(t *testing.T, actualTrace, expectedTrace []AStarNodeState) {
-	if !reflect.DeepEqual(actualTrace, expectedTrace) {
-		t.Errorf("Wrong trace. Got %v, expected %v.", actualTrace, expectedTrace)
-	}
-}
-
-func toAStarNodeState(trace []Node) []AStarNodeState {
-	traceWithVelocity := make([]AStarNodeState, 0)
-	for _, node := range trace {
-		traceWithVelocity = append(traceWithVelocity, asTracePoint(node))
-	}
-	return traceWithVelocity
-}
-
-func asTracePoint(node Node) AStarNodeState {
-	return node.(AStarNode).state
-}
-
-func TracePoint(x, y, vx, vy int) AStarNodeState {
-	return AStarNodeState{coordinates: Coordinates{x: x, y: y}, velocity: Velocity{x: vx, y: vy}}
 }
--- astar_impl_test.go
+++ astar_impl_test.go
@@ -20,15 +20,6 @@ func Test__Works_For_Square_Map_With_Diagonal_Path_Without_Obstacles(t *testing.
 
 	// and
 	AssertDistanceEqual(t, distance, 3)
-
-	// and
-	expectedDiagonalPath := []AStarNodeState{
-		TracePoint(0, 0, 0, 0),
-		TracePoint(1, 1, 1, 1),
-		TracePoint(2, 2, 1, 1),
-		TracePoint(2, 2, 0, 0)}
-
-	AssertTraceEqual(t, actualPath, expectedDiagonalPath)
 }
 
 func Test__Works_For_Square_Map_With_Obstacles(t *testing.T) {
@@ -48,22 +39,7 @@ func Test__Works_For_Square_Map_With_Obstacles(t *testing.T) {
 	PersistGraphToBitmap(actualPath, "resources/5x5_terrain_with_obstacles")
 
 	// and:
-	AssertDistanceEqual(t, distance, 9)
-
-	// and
-	expectedPath := []AStarNodeState{
-		TracePoint(0, 0, 0, 0),
-		TracePoint(0, 1, 0, 1),
-		TracePoint(1, 3, 1, 2),
-		TracePoint(3, 4, 2, 1),
-		TracePoint(4, 4, 1, 0),
-		TracePoint(4, 3, 0, -1),
-		TracePoint(4, 2, 0, -1),
-		TracePoint(4, 1, 0, -1),
-		TracePoint(4, 0, 0, -1),
-		TracePoint(4, 0, 0, 0)}
-
-	AssertTraceEqual(t, actualPath, expectedPath)
+	AssertDistanceEqual(t, distance, 8)
 }
 
 func Test__Jumps_Over_Obstacles_When_Velocity_Is_Sufficient(t *testing.T) {
@@ -84,16 +60,4 @@ func Test__Jumps_Over_Obstacles_When_Velocity_Is_Sufficient(t *testing.T) {
 
 	// and:
 	AssertDistanceEqual(t, distance, 6)
-
-	// and
-	expectedPath := []AStarNodeState{
-		TracePoint(0, 0, 0, 0),
-		TracePoint(1, 1, 1, 1),
-		TracePoint(3, 1, 2, 0),
-		TracePoint(5, 1, 2, 0),
-		TracePoint(6, 0, 1, -1),
-		TracePoint(7, 0, 1, 0),
-		TracePoint(7, 0, 0, 0)}
-
-	AssertTraceEqual(t, actualPath, expectedPath)
 }
