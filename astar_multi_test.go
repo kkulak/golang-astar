ad2295167dde6c39eac872693779a248596dbe20
Kamil Kulak <kulak.kam@gmail.com>
2017-01-21 22:24:49 +0100

    Failing multi astar 08 test

---
+++ astar_multi_test.go
@@ -0,0 +1,19 @@
+package astar
+
+import (
+	"testing"
+)
+
+func Test__08(t *testing.T) {
+	// given
+	start, end := ReadMapForMultiplePoints("resources/test08")
+
+	// when
+	distance, actualPath := AStar(start, end)
+
+	// then
+	PersistGraphWithMultiplePointsToBitmap(actualPath, "resources/test08")
+
+	// and
+	AssertDistanceEqual(t, distance, 3)
+}
